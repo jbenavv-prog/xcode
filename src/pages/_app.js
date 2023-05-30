f843adae2cc56fd3cc7f0c8c0b3b152a128c1648
Julian Camilo Benavides Vanegas <julian.127@teleperformance.com>
2023-05-29 20:45:43 -0500

    primera subida

---
+++ src/pages/_app.js
@@ -0,0 +1,5 @@
+import "./styles.css/globals.css";
+
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
---
+++ src/pages/productos/index.js
@@ -0,0 +1,130 @@
+import { useState, useEffect } from "react";
+import Layout from "../layouts";
+import { AlertaExito } from "./components/alertas";
+
+export default function Productos() {
+  const [productosEstado, ActualizarProductos] = useState(null);
+  const [alerta, actualizarAlerta] = useState(null);
+
+  useEffect(() => {
+    async function obtenerDatos() {
+      const peticion = await fetch("/productosList/productos.json");
+      const respuesta = await peticion.json();
+      ActualizarProductos(respuesta.productos);
+    }
+
+    obtenerDatos();
+  }, []);
+
+  function alertaExito() {
+    actualizarAlerta(true);
+  }
+
+  return (
+    <Layout>
+      {/* <div className="flex justify-center">
+        {alerta ? <AlertaExito></AlertaExito> : <></>}
+      </div> */}
+      <div
+        id="toast-default"
+        className="flex items-center w-full max-w-xs p-4 text-gray-500 bg-white rounded-lg shadow dark:text-gray-400 dark:bg-gray-800"
+        role="alert"
+      >
+        <div className="inline-flex items-center justify-center flex-shrink-0 w-8 h-8 text-blue-500 bg-blue-100 rounded-lg dark:bg-blue-800 dark:text-blue-200">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M12.395 2.553a1 1 0 00-1.45-.385c-.345.23-.614.558-.822.88-.214.33-.403.713-.57 1.116-.334.804-.614 1.768-.84 2.734a31.365 31.365 0 00-.613 3.58 2.64 2.64 0 01-.945-1.067c-.328-.68-.398-1.534-.398-2.654A1 1 0 005.05 6.05 6.981 6.981 0 003 11a7 7 0 1011.95-4.95c-.592-.591-.98-.985-1.348-1.467-.363-.476-.724-1.063-1.207-2.03zM12.12 15.12A3 3 0 017 13s.879.5 2.5.5c0-1 .5-4 1.25-4.5.5 1 .786 1.293 1.371 1.879A2.99 2.99 0 0113 13a2.99 2.99 0 01-.879 2.121z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+          <span className="sr-only">Fire icon</span>
+        </div>
+        <div className="ml-3 text-sm font-normal">Set yourself free.</div>
+        <button
+          type="button"
+          className="ml-auto -mx-1.5 -my-1.5 bg-white text-gray-400 hover:text-gray-900 rounded-lg focus:ring-2 focus:ring-gray-300 p-1.5 hover:bg-gray-100 inline-flex h-8 w-8 dark:text-gray-500 dark:hover:text-white dark:bg-gray-800 dark:hover:bg-gray-700"
+          data-dismiss-target="#toast-default"
+          data-collapse-toggle="toast-default"
+          aria-label="Close"
+        >
+          <span className="sr-only">Close</span>
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </button>
+      </div>
+
+      <div className="flex flex-wrap justify-between">
+        {productosEstado ? (
+          productosEstado.map((elemento, index) => {
+            return (
+              <Producto
+                key={index}
+                producto={elemento}
+                alerta={alertaExito}
+              ></Producto>
+            );
+          })
+        ) : (
+          <h3>Cargando</h3>
+        )}
+      </div>
+    </Layout>
+  );
+}
+
+export function Producto({ producto, alerta }) {
+  return (
+    <div className="mb-5 max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+      <a href="#">
+        <img className="rounded-t-lg" src={producto.imagen} alt="" />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {producto.nombre}
+          </h5>
+        </a>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">
+          {producto.descripcion}
+        </p>
+        <a
+          onClick={alerta}
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+        >
+          Comprar
+          <svg
+            aria-hidden="true"
+            className="w-4 h-4 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+}
